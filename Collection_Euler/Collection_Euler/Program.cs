9c913a2c05805dc158e3de4fc49236f8cde9cbca
Jonas Bergner <j.bergner@live.de>
2026-01-11 17:14:11 +0100

    start setup

---
+++ Collection_Euler/Collection_Euler/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Solve Problem 0
+long solveZero(int value)
+{
+    for (int i = 0; i < value; i++)
+    {
+
+    }
+}
