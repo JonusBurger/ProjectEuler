789ca4077bb31fa6dec2afca52a211f50302a95f
Jonas Bergner <j.bergner@live.de>
2026-01-20 07:54:33 +0100

    solve Problem 0-2

--- Collection_Euler/Collection_Euler/Program.cs
+++ Collection_Euler/Collection_Euler/Program.cs
@@ -1,10 +1,64 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// Solve Problem 0
-long solveZero(int value)
-{
-    for (int i = 0; i < value; i++)
-    {
+using System.Numerics;
 
+Console.WriteLine("Hello, World!");
+evenFibos(4000000);
+//Multiplesof3or5(1000);
+//solveZero(764000);
+// Solve Problem 0
+void solveZero(Int64 value)
+{
+    value = value / 2;
+    Int64 countSum = 0;
+    for (Int64 i = 1; i <= value; i++)
+    {
+        Int64 square = (2*i-1)* (2 * i - 1);
+        countSum += square;
     }
+    Console.WriteLine($"Resulting Square is: {countSum}");
+}
+
+// Solve Problem 1
+void Multiplesof3or5(long value)
+{
+    long sum = 0;
+    for (long i = 1; i < value; i++)
+    {
+        if (i % 5 == 0)
+        {
+            sum += i;
+        } else if (i % 3 == 0)
+        {
+            sum += i;
+        }
+    }
+    Console.WriteLine("Resulting Sum is: ");
+    Console.Write(sum);
+}
+
+// Solve Problem 2
+void evenFibos(int value)
+{
+    long[] fiboStorage = new long[value];
+    long fiboValue = 1;
+    long sumEven = 0;
+    int i = 1;
+    while (fiboValue < value)
+    {
+        if (i == 1 || i == 2)
+        {
+            fiboStorage[i] = fiboValue;
+        } else
+        {
+            fiboValue = fiboStorage[i - 1] + fiboStorage[i - 2];
+            fiboStorage[i] = fiboValue;
+        }
+        if (fiboValue % 2 == 0)
+        {
+            sumEven += fiboValue;
+        }
+        i++;
+    }
+
+    Console.WriteLine($"Sum of all the even fibonacci Numbers that don´t exceed {value} is {sumEven}");
 }
--- Collection_Euler/Collection_Euler/obj/Release/net10.0/Collection_Euler.AssemblyInfo.cs
+++ Collection_Euler/Collection_Euler/obj/Release/net10.0/Collection_Euler.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Collection_Euler")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c913a2c05805dc158e3de4fc49236f8cde9cbca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Collection_Euler")]
